3b9f791e7034abd83589d02e40695f1d972c045d
CarolShine <carolinecg431@gmail.com>
2021-05-22 09:51:13 -0300

    Criei um testNumeros usando o bibNumeros

---
+++ testNumeros.py
@@ -0,0 +1,18 @@
+import bibNumeros
+
+if(bibNumeros.testaMultiplo4(4) == True):
+    print("Teste 1 Ok")
+else:
+    print("Teste 1 falhou")
+
+if(bibNumeros.testaMultiplo4(7) == False):
+    print("Teste 2 ok")
+    
+else:
+    print("Teste 2 falhou")
+
+if(bibNumeros.testaMultiplo4(-16) == True):
+    print("Teste 3 ok")
+else:
+    print("Teste 3 falhou")
+    
