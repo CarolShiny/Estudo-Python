68c397e4282c787da4d58b9424453f1514efbc39
CarolShine <carolinecg431@gmail.com>
2021-05-22 09:35:49 -0300

    Criei um bibLista

---
+++ bibListas.py
@@ -0,0 +1,26 @@
+def somaLista (lista):
+  soma = 0
+  for i in range (len(lista)):
+    soma += lista[i]
+    
+  return soma
+
+def maiorLista(lista):
+    lista.sort()
+    return lista
+
+def testPar (numero):
+    if(numero %2 == 0):
+        return true
+    else:
+        return false
+
+def media (idade):
+    soma = 0
+    for i in range (len(lista)):
+        soma += lista[i]
+    
+    
+        
+    
+    
